ebdd2e726056f421a724a880734ce07a81010d77
Sergey Parshukov <codedby@bugman.me>
2018-04-09 00:18:36 +0300

    Basic unified Feed parsing

---
+++ src/lib.rs
@@ -0,0 +1,143 @@
+extern crate atom_syndication as atom;
+extern crate rss;
+
+use std::str::FromStr;
+use std::error::Error;
+
+#[derive(Debug)]
+pub struct Feed {
+    title: String,
+    description: Option<String>,
+    last_update: Option<String>,
+    items: Vec<Item>,
+}
+
+#[derive(Debug)]
+pub struct Item {
+    title: Option<String>,
+    pub_date: Option<String>,
+    link: Option<String>,
+}
+
+impl FromStr for Feed {
+    type Err = Box<Error>;
+
+    fn from_str(source: &str) -> Result<Self, Self::Err> {
+        let s = Syndication::from(source)?;
+        let f = Feed::from(s);
+        Ok(f)
+    }
+}
+
+impl From<atom::Feed> for Feed {
+    fn from(src: atom::Feed) -> Self {
+        Feed {
+            title: src.title().to_owned(),
+            description: src.subtitle().map(|x| x.to_owned()),
+            last_update: Some(src.updated().to_owned()),
+            items: items_from_atom(&src),
+        }
+    }
+}
+
+impl From<rss::Channel> for Feed {
+    fn from(src: rss::Channel) -> Self {
+        Feed {
+            title: src.title().to_owned(),
+            description: Some(src.description().to_owned()),
+            last_update: src.last_build_date().map(|x| x.to_owned()),
+            items: items_from_rss(&src),
+        }
+    }
+}
+
+impl<'a> From<&'a rss::Item> for Item {
+    fn from(src: &rss::Item) -> Self {
+        Item {
+            title: src.title().map(|x| x.to_owned()),
+            pub_date: src.pub_date().map(|x| x.to_owned()),
+            link: src.link().map(|x| x.to_owned()),
+        }
+    }
+}
+
+impl<'a> From<&'a atom::Entry> for Item {
+    fn from(src: &atom::Entry) -> Self {
+        Item {
+            title: Some(src.title().to_owned()),
+            pub_date: src.published().map(|x| x.to_owned()),
+            link: link_from_alternate(src.links()),
+        }
+    }
+}
+
+fn link_from_alternate(links: &[atom::Link]) -> Option<String> {
+    for link in links.iter() {
+        if link.rel() == "alternate" {
+            return Some(link.href().to_owned())
+        }
+    }
+    None
+}
+
+// // TODO: replace with functional-style conversion
+// fn link_from_content(src: Option<&atom::Content>) -> Option<String> {
+//     match src {
+//         None => None,
+//         Some(c) => c.src().map(|x| x.to_owned()),
+//     }
+// }
+
+// TODO: replace with functional-style conversion
+fn items_from_rss(src: &rss::Channel) -> Vec<Item> {
+    let mut result = Vec::with_capacity(src.items().len());
+    for x in src.items().iter() {
+        let item = Item::from(x);
+        result.push(item);
+    }
+    result
+}
+
+// TODO: replace with functional-style conversion
+fn items_from_atom(src: &atom::Feed) -> Vec<Item> {
+    let mut result = Vec::with_capacity(src.entries().len());
+    for x in src.entries().iter() {
+        let item = Item::from(x);
+        result.push(item);
+    }
+    result
+}
+
+// impl ToOwned for Option<&str> {  // TODO: lifetime
+//     fn to_owned(&self) -> Self::Owned {
+//         &self.map(|x| x.to_owned())
+//     }
+// }
+
+#[derive(Debug)]
+pub enum Syndication {
+    Atom(atom::Feed),
+    RSS(rss::Channel),
+}
+
+impl Syndication {
+    pub fn from(s: &str) -> Result<Syndication, Box<Error>> {
+        match s.parse::<atom::Feed>() {
+            Ok(feed) => Ok(Syndication::Atom(feed)),
+            Err(atom::Error::InvalidStartTag) => match s.parse::<rss::Channel>() {
+                Ok(channel) => Ok(Syndication::RSS(channel)),
+                Err(err) => Err(err.into()),
+            },
+            Err(err) => Err(err.into()),
+        }
+    }
+}
+
+impl From<Syndication> for Feed {
+    fn from(s: Syndication) -> Self {
+        match s {
+            Syndication::Atom(x) => Feed::from(x),
+            Syndication::RSS(x) => Feed::from(x),
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate feeds_to_instapaper;
+
+use std::fs::File;
+use std::io::Read;
+use std::error::Error;
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("error: {}", err);
+        std::process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<Error>> {
+    let _filename = "samples/junk.xml"; // should fail
+    let _filename = "samples/ghc.xml";  // RSS
+    let _filename = "samples/pike.xml"; // Atom
+
+    let mut file = File::open(_filename)?;
+    let mut text = String::new();
+    file.read_to_string(&mut text)?;
+
+    let feed = text.parse::<feeds_to_instapaper::Feed>()?;
+    println!("{:#?}", feed);
+    Ok(())
+}
