a3df0d9dc979ba9ac4c5977db705d096e2523306
Sergey Parshukov <codedby@bugman.me>
2018-04-09 22:17:53 +0300

    Cleaner Option<&str> -> Option<String> conversion

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,7 @@ impl From<atom::Feed> for Feed {
     fn from(src: atom::Feed) -> Self {
         Feed {
             title: src.title().to_owned(),
-            description: src.subtitle().map(|x| x.to_owned()),
+            description: src.subtitle().map(str::to_owned),
             last_update: Some(src.updated().to_owned()),
             items: src.entries().iter().map(Item::from).collect(),
         }
@@ -45,7 +45,7 @@ impl From<rss::Channel> for Feed {
         Feed {
             title: src.title().to_owned(),
             description: Some(src.description().to_owned()),
-            last_update: src.last_build_date().map(|x| x.to_owned()),
+            last_update: src.last_build_date().map(str::to_owned),
             items: src.items().iter().map(Item::from).collect(),
         }
     }
@@ -54,9 +54,9 @@ impl From<rss::Channel> for Feed {
 impl<'a> From<&'a rss::Item> for Item {
     fn from(src: &rss::Item) -> Self {
         Item {
-            title: src.title().map(|x| x.to_owned()),
-            pub_date: src.pub_date().map(|x| x.to_owned()),
-            link: src.link().map(|x| x.to_owned()),
+            title: src.title().map(str::to_owned),
+            pub_date: src.pub_date().map(str::to_owned),
+            link: src.link().map(str::to_owned),
         }
     }
 }
@@ -65,7 +65,7 @@ impl<'a> From<&'a atom::Entry> for Item {
     fn from(src: &atom::Entry) -> Self {
         Item {
             title: Some(src.title().to_owned()),
-            pub_date: src.published().map(|x| x.to_owned()),
+            pub_date: src.published().map(str::to_owned),
             link: link_from_alternate(src.links()),
         }
     }
@@ -79,20 +79,6 @@ fn link_from_alternate(links: &[atom::Link]) -> Option<String> {
     }
     None
 }
-
-// // TODO: replace with functional-style conversion
-// fn link_from_content(src: Option<&atom::Content>) -> Option<String> {
-//     match src {
-//         None => None,
-//         Some(c) => c.src().map(|x| x.to_owned()),
-//     }
-// }
-
-// impl ToOwned for Option<&str> {  // TODO: lifetime
-//     fn to_owned(&self) -> Self::Owned {
-//         &self.map(|x| x.to_owned())
-//     }
-// }
 
 #[derive(Debug)]
 pub enum Syndication {
