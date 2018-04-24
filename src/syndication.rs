cbd43f30f26b59ed4f45dc6abc57b3cb2965ea7c
Sergey Parshukov <codedby@bugman.me>
2018-04-24 20:42:43 +0300

    Attempt on parsing to actual Urls in try_from for the Feed

--- src/syndication.rs
+++ src/syndication.rs
@@ -3,78 +3,113 @@ use std::error::Error;
 
 use atom_syndication as atom;
 use rss;
+use url::Url;
+use try_from::TryFrom;
 
 #[derive(Debug)]
 pub struct Feed {
     pub title: String,
     pub description: Option<String>,
-    pub last_update: Option<String>,
+    pub last_update: Option<String>, // TODO: convert to a date?
     pub items: Vec<Item>,
 }
 
 #[derive(Debug)]
 pub struct Item {
     pub title: Option<String>,
-    pub pub_date: Option<String>,
-    pub link: Option<String>,
+    pub pub_date: Option<String>, // TODO: convert to a date?
+    pub link: Option<String>,     // TODO: in process of converting to an url
+    url: Option<Url>,             // TODO: ^
 }
 
 impl FromStr for Feed {
     type Err = Box<Error>;
 
     fn from_str(src: &str) -> Result<Self, Self::Err> {
-        rss::Channel::from_str(src)
-            .map(Feed::from)
-            .or_else(|err| match err {
-                rss::Error::InvalidStartTag => atom::Feed::from_str(src)
-                    .map(Feed::from)
-                    .map_err(Self::Err::from),
-                e => Err(e).map_err(Self::Err::from),
-            })
-    }
-}
-
-impl From<atom::Feed> for Feed {
-    fn from(src: atom::Feed) -> Self {
-        Feed {
-            title: src.title().to_owned(),
-            description: src.subtitle().map(str::to_owned),
-            last_update: Some(src.updated().to_owned()),
-            items: src.entries().iter().map(Item::from).collect(),
+        match rss::Channel::from_str(src) {
+            Ok(chan) => Feed::try_from(chan),
+            Err(rss::Error::InvalidStartTag) => match atom::Feed::from_str(src) {
+                Ok(feed) => Feed::try_from(feed),
+                Err(err) => Err(err.into()),
+            },
+            Err(err) => Err(err.into()),
         }
     }
 }
 
-impl From<rss::Channel> for Feed {
-    fn from(src: rss::Channel) -> Self {
-        Feed {
-            title: src.title().to_owned(),
-            description: Some(src.description().to_owned()),
-            last_update: src.last_build_date().map(str::to_owned),
-            items: src.items().iter().map(Item::from).collect(),
-        }
+impl TryFrom<atom::Feed> for Feed {
+    type Err = Box<Error>;
+
+    fn try_from(src: atom::Feed) -> Result<Self, Self::Err> {
+        let title = src.title().to_owned();
+        let description = src.subtitle().map(str::to_owned);
+        let last_update = Some(src.updated().to_owned());
+        let items: Result<Vec<Item>, _> = src.entries().into_iter().map(Item::try_from).collect();
+        let items = items?;
+        Ok(Feed {
+            title,
+            description,
+            last_update,
+            items,
+        })
     }
 }
 
-impl<'a> From<&'a rss::Item> for Item {
-    fn from(src: &rss::Item) -> Self {
-        Item {
+impl TryFrom<rss::Channel> for Feed {
+    type Err = Box<Error>;
+
+    fn try_from(src: rss::Channel) -> Result<Self, Self::Err> {
+        let title = src.title().to_owned();
+        let description = Some(src.description().to_owned());
+        let last_update = src.last_build_date().map(str::to_owned);
+        let items: Result<Vec<Item>, _> = src.items().into_iter().map(Item::try_from).collect();
+        let items = items?;
+        Ok(Feed {
+            title,
+            description,
+            last_update,
+            items,
+        })
+    }
+}
+
+impl<'a> TryFrom<&'a rss::Item> for Item {
+    type Err = Box<Error>;
+
+    fn try_from(src: &rss::Item) -> Result<Self, Self::Err> {
+        let url = parse_url(src.link())?;
+        let result = Item {
             title: src.title().map(str::to_owned),
             pub_date: src.pub_date().map(str::to_owned),
             link: src.link().map(str::to_owned),
-        }
+            url,
+        };
+        Ok(result)
     }
 }
 
-impl<'a> From<&'a atom::Entry> for Item {
-    fn from(src: &atom::Entry) -> Self {
-        Item {
+impl<'a> TryFrom<&'a atom::Entry> for Item {
+    type Err = Box<Error>;
+
+    fn try_from(src: &atom::Entry) -> Result<Self, Self::Err> {
+        let link = src.links()
+            .iter()
+            .find(|link| link.rel() == "alternate")
+            .map(|link| link.href());
+        let url = parse_url(link)?;
+        let result = Item {
             title: Some(src.title().to_owned()),
             pub_date: src.published().map(str::to_owned),
-            link: src.links()
-                .iter()
-                .find(|link| link.rel() == "alternate")
-                .map(|link| link.href().to_owned()),
-        }
+            link: link.map(str::to_owned),
+            url,
+        };
+        Ok(result)
+    }
+}
+
+fn parse_url(src: Option<&str>) -> Result<Option<Url>, Box<Error>> {
+    match src {
+        Some(link) => Url::parse(link).map(Some).map_err(|e| e.into()),
+        None => Ok(None),
     }
 }
